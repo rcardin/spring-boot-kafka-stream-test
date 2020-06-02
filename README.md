3ea849d1163ccfc55319a12b645cfc33a0927cfb
rcardin <riccardo.cardin@gmail.com>
2020-06-02 15:35:04 +0200

    Removed Spring context configuration in test (not needed)

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ In details, Spring Boot manages the creation of Kafka Streams. The library
 [kafka-streams-test-utils](https://kafka.apache.org/documentation/streams/developer-guide/testing.html) 
 manages the interaction with the broker.
 
-The good part is that no broker starts during tests, not even an embedded Kafka :)
+The good part is that no broker starts during tests, not even an embedded Kafka :) 
+Moreover, the test is a pure JUnit test and requires no Spring context configuration.
--- src/test/java/io/rcardin/spring/kafka/stream/ApplicationTests.java
+++ src/test/java/io/rcardin/spring/kafka/stream/ApplicationTests.java
@@ -4,35 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Properties;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.TestOutputTopic;
 import org.apache.kafka.streams.TopologyTestDriver;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.NONE
-)
 class ApplicationTests {
   
-  @Autowired
-  private StreamsBuilderFactoryBean streamsBuilderFactoryBean;
   private TestInputTopic<String, String> inputTopic;
   private TestOutputTopic<String, Long> outputTopic;
   private TopologyTestDriver testDriver;
   
   @BeforeEach
   void setUp() {
+    final StreamsBuilder builder = configureTopology();
+    Properties props = buildStreamDummyConfiguration();
     testDriver = new TopologyTestDriver(
-        streamsBuilderFactoryBean.getTopology(),
-        Objects.requireNonNull(streamsBuilderFactoryBean.getStreamsConfiguration())
+        builder.build(),
+        Objects.requireNonNull(props)
     );
     inputTopic = testDriver.createInputTopic("words",
         Serdes.String().serializer(),
@@ -42,13 +38,29 @@ class ApplicationTests {
         Serdes.String().deserializer(),
         Serdes.Long().deserializer()
     );
-    
+  }
+  
+  private StreamsBuilder configureTopology() {
+    final Application application = new Application();
+    final StreamsBuilder builder = new StreamsBuilder();
+    application.kStreamWordCounter(builder);
+    return builder;
+  }
+  
+  private Properties buildStreamDummyConfiguration() {
+    Properties props = new Properties();
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.Serdes$StringSerde");
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.Serdes$StringSerde");
+    return props;
   }
   
   @AfterEach
   void tearDown() {
     testDriver.close();
-    streamsBuilderFactoryBean.getKafkaStreams().close();
   }
   
   @Test
